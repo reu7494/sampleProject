cf830849e34bf33228f74deb5ed73858b8d78733
black-ne-ko <reu7494@gmail.com>
2025-04-23 11:03:45 +0900

    my-Diary

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,16 +1,25 @@
 import Lamborghini from "./images/Lamborghini.jpg";
 
+const Cars = {
+  name: "Lamborghini",
+  imageSize: "s",
+  theme: {
+    backgroundColor: "black",
+    color: "red",
+  },
+};
+
 function Car() {
   return <img src={Lamborghini} alt="Lamborghini" />;
 }
 
 export default function Gallery() {
   return (
-    <section>
-      <h1>Nice Car!</h1>
+    <div style={Cars.theme}>
+      <h1>{Cars.name} is Nice Car!</h1>
       <Car />
       <Car />
       <Car />
-    </section>
+    </div>
   );
 }

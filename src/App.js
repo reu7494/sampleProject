a8075a41edf810537f0bb5724524589587f400ed
black-ne-ko <reu7494@naver.com>
2025-04-28 11:37:58 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import Car from "./images/Lamborghini.jpg";
+import { people } from "./data.js";
+import { getImageUrl } from "./utils.js";
 
-function Cars({ name, size = 70 }) {
+function ListItems({ people, title }) {
   return (
-    <div>
-      <h1>{name} is Nice Car!</h1>
-      <img src={Car} alt={name} width={size} height={size} />
-    </div>
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>
+              {" " + person.profession + " "}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default function Image() {
-  return <Cars name="Lamborghini" />;
+export default function List() {
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const everyoneElse = people.filter(
+    (person) => person.profession !== "chemist"
+  );
+
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <ListItems people={chemists} title="Chemists" />
+      <ListItems people={everyoneElse} title="EveryoneElse" />
+    </article>
+  );
 }

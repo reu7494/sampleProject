2aec98540dee4506dc2f4ec30b2b21d120a425a0
black-ne-ko <reu7494@naver.com>
2025-04-29 10:59:51 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import { recipes } from "./data.js";
-
-function Recipe({ id, name, ingredients }) {
-  return (
-    <div>
-      <div>
-        <h2>{name}</h2>
-        <ul>
-          <li key={id}>{ingredients}</li>
-        </ul>
-      </div>
-    </div>
-  );
+function TestList({ teli }) {
+  return <h2>Test List {teli}</h2>;
 }
 
-export default function RecipeList() {
-  const recipe = recipes;
-  return (
-    <div>
-      <h1>Recipes</h1>
-      <Recipe
-        id={recipe.id}
-        name={recipe.name}
-        ingredients={recipe.ingredients}
-      />
-    </div>
-  );
+export default function Test() {
+  let list = [];
+  for (let i = 0; i < 10; i++) {
+    list.push(<TestList key={i} teli={i} />);
+  }
+  return list;
 }

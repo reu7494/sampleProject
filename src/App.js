54e78c9332733062d7ab8516b22bd3aaaecfc072
black-ne-ko <reu7494@naver.com>
2025-04-30 14:03:37 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-export default function App() {
-  return (
-    <MyButton
-      onPlay={() => alert("Playing")}
-      onUpload={() => alert("Upload")}
-    />
-  );
-}
-
-function MyButton({ onPlay, onUpload }) {
-  return (
-    <>
-      <Button onClick={onPlay}>Play Movie</Button>
-      <Button onClick={onUpload}>Upload Image</Button>
-    </>
-  );
-}
-
 function Button({ onClick, children }) {
   return <button onClick={onClick}>{children}</button>;
+}
+
+function PlayButton({ movieName }) {
+  return (
+    <Button onClick={() => alert(`Playing ${movieName}`)}>
+      Play {movieName}
+    </Button>
+  );
+}
+
+function UploadButton() {
+  return <Button onClick={() => alert("Uploading")}>Upload Image</Button>;
+}
+
+export default function Toolbar() {
+  return (
+    <div>
+      <PlayButton movieName="Kiki's Delivery Service"></PlayButton>
+      <UploadButton />
+    </div>
+  );
 }

6138d0ef213c3d909fe38daa85cd8a66319dc4d6
black-ne-ko <reu7494@naver.com>
2025-05-07 15:57:35 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -7,40 +7,31 @@ export default function Scoreboard() {
     score: 10,
   });
 
-  function handlePlusClick() {
-    setPlayer({
-      ...player,
-      score: player.score + 1,
-    });
-  }
-
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
-
-  function handleLastNameChange(e) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value,
-    });
+  function handleClicker(target) {
+    return function (e) {
+      setPlayer({
+        ...player,
+        [target]:
+          typeof player[target] === "number"
+            ? player[target] + 1
+            : e.target.value,
+      });
+    };
   }
 
   return (
     <>
       <label>
         Score: <b>{player.score}</b>{" "}
-        <button onClick={handlePlusClick}>+1</button>
+        <button onClick={handleClicker("score")}>+1</button>
       </label>
       <label>
         First name:
-        <input value={player.firstName} onChange={handleFirstNameChange} />
+        <input value={player.firstName} onChange={handleClicker("firstName")} />
       </label>
       <label>
         Last name:
-        <input value={player.lastName} onChange={handleLastNameChange} />
+        <input value={player.lastName} onChange={handleClicker("lastName")} />
       </label>
     </>
   );

adf29d0ea2a3460b9017ef3793fdc63d6f36b580
black-ne-ko <reu7494@naver.com>
2025-05-08 11:25:39 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useImmer } from "use-immer";
 import Background from "./Background.js";
 import Box from "./Box.js";
 
@@ -8,19 +9,16 @@ const initialPosition = {
 };
 
 export default function Canvas() {
-  const [shape, setShape] = useState({
+  const [shape, setShape] = useImmer({
     color: "orange",
     position: initialPosition,
   });
 
   function handleMove(dx, dy) {
-    setShape((e) => ({
-      ...e,
-      position: {
-        x: (e.position.x += dx),
-        y: (e.position.y += dy),
-      },
-    }));
+    setShape((draft) => {
+      draft.position.x += dx;
+      draft.position.y += dy;
+    });
   }
 
   function handleColorChange(e) {

6bc3ea1a370e4cdcaa04e8b815be29e8393dc021
black-ne-ko <reu7494@naver.com>
2025-05-10 20:28:17 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,77 +1,44 @@
 import { useState } from "react";
+import AddTodo from "./AddTodo.js";
+import TaskList from "./TaskList.js";
 
-const initialProducts = [
-  {
-    id: 0,
-    name: "Baklava",
-    count: 1,
-  },
-  {
-    id: 1,
-    name: "Cheese",
-    count: 5,
-  },
-  {
-    id: 2,
-    name: "Spaghetti",
-    count: 2,
-  },
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: "Buy milk", done: true },
+  { id: 1, title: "Eat tacos", done: false },
+  { id: 2, title: "Brew tea", done: false },
 ];
 
-export default function ShoppingCart() {
-  const [products, setProducts] = useState(initialProducts);
+export default function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos);
 
-  function handleIncreaseClick(productId) {
-    setProducts(
-      products.map((product) => {
-        if (product.id === productId) {
-          return {
-            ...product,
-            count: product.count + 1,
-          };
-        } else {
-          return product;
-        }
-      })
-    );
+  function handleAddTodo(title) {
+    todos.push({
+      id: nextId++,
+      title: title,
+      done: false,
+    });
   }
 
-  function handleDeleteClick(productId) {
-    let nextProducts = products.map((product) => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          count: product.count - 1,
-        };
-      } else {
-        return product;
-      }
-    });
-    nextProducts = nextProducts.filter((p) => p.count > 0);
-    setProducts(nextProducts);
+  function handleChangeTodo(nextTodo) {
+    const todo = todos.find((t) => t.id === nextTodo.id);
+    todo.title = nextTodo.title;
+    todo.done = nextTodo.done;
+  }
+
+  function handleDeleteTodo(todoId) {
+    const index = todos.findIndex((t) => t.id === todoId);
+    todos.splice(index, 1);
   }
 
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          {product.name} (<b>{product.count}</b>)
-          <button
-            onClick={() => {
-              handleIncreaseClick(product.id);
-            }}
-          >
-            +
-          </button>
-          <button
-            onClick={() => {
-              handleDeleteClick(product.id);
-            }}
-          >
-            –
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
   );
 }

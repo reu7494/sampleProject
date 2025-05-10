4e98060e18d95d16fa294e63f11faf71150853d0
black-ne-ko <reu7494@naver.com>
2025-05-10 20:45:24 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -13,22 +13,30 @@ export default function TaskApp() {
   const [todos, setTodos] = useState(initialTodos);
 
   function handleAddTodo(title) {
-    todos.push({
-      id: nextId++,
-      title: title,
-      done: false,
-    });
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false,
+      },
+    ]);
   }
 
   function handleChangeTodo(nextTodo) {
-    const todo = todos.find((t) => t.id === nextTodo.id);
-    todo.title = nextTodo.title;
-    todo.done = nextTodo.done;
+    setTodos(
+      todos.map((t) => {
+        if (t.id === nextTodo.id) {
+          return nextTodo;
+        } else {
+          return t;
+        }
+      })
+    );
   }
 
   function handleDeleteTodo(todoId) {
-    const index = todos.findIndex((t) => t.id === todoId);
-    todos.splice(index, 1);
+    setTodos(todos.filter((t) => t.id !== todoId.id));
   }
 
   return (

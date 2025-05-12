bad2c4b9bfb9463da0b7f45213be34a57d8af565
black-ne-ko <reu7494@naver.com>
2025-05-12 11:16:32 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import { useState } from "react";
+
 export default function EditProfile() {
+  const [state, setState] = useState(false);
+  const [firstName, setFirstName] = useState("Jane");
+  const [lastName, setLastName] = useState("Jacobs");
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setState(!state);
+      }}
+    >
       <label>
-        First name: <b>Jane</b>
-        <input />
+        First name:
+        {state ? (
+          <input
+            value={firstName}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{firstName}</b>
+        )}
       </label>
       <label>
-        Last name: <b>Jacobs</b>
-        <input />
+        Last name:
+        {state ? (
+          <input
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{lastName}</b>
+        )}
       </label>
-      <button type="submit">Edit Profile</button>
+      <button type="submit">{state ? "Save" : "Edit"} Profile</button>
       <p>
-        <i>Hello, Jane Jacobs!</i>
+        <i>
+          Hello, {firstName} {lastName}!
+        </i>
       </p>
     </form>
   );

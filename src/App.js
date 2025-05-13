e8827e6e7cee162bdba53f4dd4194f21153eb9d6
black-ne-ko <reu7494@naver.com>
2025-05-13 21:47:10 +0900

    my-RockPaperScissors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import { useState } from "react";
-export default function DarkMode() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+export default function RockPaperScissors() {
+  const choices = ["가위", "바위", "보"];
+  const [userChoices, setUserChoices] = useState();
+  const [computerChoices, setComputerChoices] = useState();
+  const [result, setResult] = useState();
 
-  function ChangeColor() {
-    setIsDarkMode(!isDarkMode);
+  function playGame(userPick) {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const computerPick = choices[randomIndex];
+    if (computerPick === userPick) {
+      setResult("무승부");
+    } else if (
+      (userPick === "가위" && computerPick === "보") ||
+      (userPick === "바위" && computerPick === "가위") ||
+      (userPick === "보" && computerPick === "바위")
+    ) {
+      setResult("유저 승리");
+    } else {
+      setResult("컴퓨터 승리");
+    }
+    setUserChoices(userPick);
+    setComputerChoices(computerPick);
   }
-
-  const appClassName = `app ${isDarkMode ? "dark" : "light"}`;
-
   return (
-    <div className={appClassName}>
-      <h1>{isDarkMode ? "다크모드" : "라이트모드"}</h1>
-      <button onClick={ChangeColor}>
-        {isDarkMode ? "라이트모드 전환" : "다크모드 전환"}
-      </button>
-    </div>
+    <>
+      <h2>가위 바위 보 게임</h2>
+      <div className="button">
+        {choices.map((choices) => (
+          <button key={choices} onClick={() => playGame(choices)}>
+            {choices}
+          </button>
+        ))}
+      </div>
+
+      <div className="result">
+        <p>나의 선택: {userChoices}</p>
+        <p>컴퓨터의 선택: {computerChoices}</p>
+        <p
+          className={`result-text ${
+            result === "유저 승리"
+              ? "win"
+              : result === "컴퓨터 승리"
+              ? "lose"
+              : "draw"
+          }`}
+        >
+          {result}
+        </p>
+      </div>
+    </>
   );
 }

3bcf7bf37fa9dc7ed0598350e558497baaac9934
black-ne-ko <reu7494@naver.com>
2025-05-15 22:11:54 +0900

    my-Diary

--- src/App.js
+++ src/App.js
@@ -1,58 +1,44 @@
 import { useState } from "react";
-import AddItem from "./AddItem.js";
-import PackingList from "./PackingList.js";
+import { initialLetters } from "./data.js";
+import Letter from "./Letter.js";
 
-let nextId = 3;
-const initialItems = [
-  { id: 0, title: "Warm socks", packed: true },
-  { id: 1, title: "Travel journal", packed: false },
-  { id: 2, title: "Watercolors", packed: false },
-];
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedLetter, setHighlightedLetter] = useState(null);
 
-export default function TravelPlan() {
-  const [items, setItems] = useState(initialItems);
-  const total = items.length;
-  const packed = items.filter((item) => item.packed).length;
-
-  function handleAddItem(title) {
-    setItems([
-      ...items,
-      {
-        id: nextId++,
-        title: title,
-        packed: false,
-      },
-    ]);
+  function handleHover(letter) {
+    setHighlightedLetter(letter);
   }
 
-  function handleChangeItem(nextItem) {
-    setItems(
-      items.map((item) => {
-        if (item.id === nextItem.id) {
-          return nextItem;
+  function handleStar(starred) {
+    setLetters(
+      letters.map((letter) => {
+        if (letter.id === starred.id) {
+          return {
+            ...letter,
+            isStarred: !letter.isStarred,
+          };
         } else {
-          return item;
+          return letter;
         }
       })
     );
   }
 
-  function handleDeleteItem(itemId) {
-    setItems(items.filter((item) => item.id !== itemId));
-  }
-
   return (
     <>
-      <AddItem onAddItem={handleAddItem} />
-      <PackingList
-        items={items}
-        onChangeItem={handleChangeItem}
-        onDeleteItem={handleDeleteItem}
-      />
-      <hr />
-      <b>
-        {packed} out of {total} packed!
-      </b>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={letter === highlightedLetter}
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
     </>
   );
 }

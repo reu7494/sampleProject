229feb0d59b6e594066b72295b002c9eb91b3fc3
black-ne-ko <reu7494@naver.com>
2025-05-16 11:39:17 +0900

    my-Diary

--- src/Letter.js
+++ src/Letter.js
@@ -1,27 +1,16 @@
-export default function Letter({
-  letter,
-  isHighlighted,
-  onHover,
-  onToggleStar,
-}) {
+export default function Letter({ letter, onToggle, isSelected }) {
   return (
-    <li
-      className={isHighlighted ? "highlighted" : ""}
-      onFocus={() => {
-        onHover(letter);
-      }}
-      onPointerMove={() => {
-        onHover(letter);
-      }}
-    >
-      <button
-        onClick={() => {
-          onToggleStar(letter);
-        }}
-      >
-        {letter.isStarred ? "Unstar" : "Star"}
-      </button>
-      {letter.subject}
+    <li className={isSelected ? "selected" : ""}>
+      <label>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={() => {
+            onToggle(letter.id);
+          }}
+        />
+        {letter.subject}
+      </label>
     </li>
   );
 }

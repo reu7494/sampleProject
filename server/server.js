2edd6f58777e3ddba33b747eae3c3c10bc7caa50
black-ne-ko <reu7494@naver.com>
2025-05-28 21:16:23 +0900

    서버 생성 중

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const cors = require("cors");
+const mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "ccia",
+  password: "Ksarohb220**",
+  database: "checkListDB",
 });
 
-app.listen(PORT, () => {
-  console.log(`Server on: http://localhost:${PORT}`);
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL 연결 실패:", err);
+  } else {
+    console.log("MySQL 연결 성공");
+  }
 });
